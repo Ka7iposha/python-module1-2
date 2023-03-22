10286993cf39cb78c8134d859d7797196608f56c
Ka7iposha <mixail.sakulin@bk.ru>
2023-03-22 17:41:25 +0300

    module1.2(task1)

---
+++ Module-1.2/task1.py
@@ -0,0 +1,61 @@
+animals = [
+    ['курочка', 'курочку', '{animal} по зёрнышку кудах-тах-тах'],
+    ['уточка', 'уточку', '{animal} та-ти-та-та'],
+    ['индюшонок', 'индюшонка', '{animal} фалды-балды'],
+    ['кисуня', 'кисоньку', '{animal} мяу-мяу'],
+    ['собачонка', 'собачонку', '{animal} гав-гав'],
+    ['коровёнка', 'коровёнку', '{animal} муки-муки'],
+    ['поросёнок', 'поросёнка', '{animal} хрюки-хрюки'],
+    ['телевизор', 'телевизор', '{animal} надо, надо, ведь у нас такое стадо']
+]
+
+first = 0
+second = 1
+third = 2
+repetitions = 2
+transfer = '\n'
+ask = 'Бабушка, бабушка, купим {animal}!'
+
+
+def question(animal_a, times):
+    result = ''
+    line = ask.format(animal=animals[animal_a][second])
+    for i in range(0, times):
+        result += line + transfer
+    return result
+
+
+def ending(length):
+    return length == len(animals) - 1
+
+
+def capital_letters(word, phrase):
+    return phrase if phrase.find(word) \
+        else phrase.replace(word, word.title())
+
+
+def after_question(number_animal):
+    result = ''
+    for i in reversed(range(0, number_animal + 1)):
+        phrase_animals = animals[i][third].format(animal=animals[i][first])
+        phrase_animals = capital_letters(animals[i][first], phrase_animals)
+        result += phrase_animals
+        if ending(i):
+            result += '!'
+            break
+        result += (',' + transfer) if i else ('.' + transfer)
+    return result
+
+
+def song_result():
+    result = ''
+
+    for i in range(0, len(animals)):
+        result += question(i, repetitions)
+        result += after_question(i)
+        result += transfer
+    return result
+
+
+print(song_result())
+
