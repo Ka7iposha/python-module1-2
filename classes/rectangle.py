487483365b90373e57d2f7124e6c1988ba09f7dc
Ka7iposha <mixail.sakulin@bk.ru>
2023-05-13 12:55:56 +0300

    Update rectangle.py

--- classes/rectangle.py
+++ classes/rectangle.py
@@ -1,10 +1,24 @@
-class Rectangle():
+class NonNegative:
+
+    def __set__(self, instance, value):
+        if value <= 0:
+            raise ValueError("значение должно быть больше 0")
+        instance.__dict__[self.name] = value
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+
+class Rectangle:
+    width = NonNegative()
+    height = NonNegative()
+
     def __init__(self, width, height):
         self._width = width
         self._height = height
 
     def perimeter(self):
-        return self._width*2 + self._height*2
+        return self._width * 2 + self._height * 2
 
     def square(self):
         return self._width * self._height
@@ -12,30 +26,9 @@ class Rectangle():
     def info(self):
         return self._width, self._height, self.perimeter(), self.square()
 
-    @property
-    def width(self):
-        return self._width
-
-    @width.setter
-    def width(self, value):
-        if value <= 0:
-            raise ValueError("ширина должна быть больше 0")
-        self._width = value
-
-
-    @property
-    def height(self):
-        return self._height
-
-    @height.setter
-    def height(self, value):
-        if value <= 0:
-            raise ValueError("высота должна быть больше 0")
-        self._height = value
-
 
 r = Rectangle(3, 4)
-print(r.width)
 
-r.width = -1
-print(r.width)
+r.height = 0
+print(r.height)
+
